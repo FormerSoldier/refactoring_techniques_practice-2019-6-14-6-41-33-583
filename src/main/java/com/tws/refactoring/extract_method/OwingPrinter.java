0fbc256a0bf6643b499bd648bff5445fe32dbc29
GARSON ZHANG (ITA-ISDC-ISD-OOCLL/ZHA) <ZHANGGA5@OOCL.COM>
2019-07-15 16:57:45 +0800

    Refactor: refactor the printOwing to split two function

--- src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
+++ src/main/java/com/tws/refactoring/extract_method/OwingPrinter.java
@@ -10,21 +10,26 @@ public class OwingPrinter {
         Iterator<Order> elements = orders.iterator();
         double outstanding = 0.0;
 
-        // print banner
-        System.out.println ("*****************************");
-        System.out.println ("****** Customer totals ******");
-        System.out.println ("*****************************");
+        printBanner();
 
-        // print owings
         while (elements.hasNext()) {
             Order each = (Order) elements.next();
             outstanding += each.getAmount();
         }
+        printDetails(name, outstanding);
+    }
 
-        // print details
+    private void printBanner(){
+        System.out.println ("*****************************");
+        System.out.println ("****** Customer totals ******");
+        System.out.println ("*****************************");
+    }
+
+    private void printDetails(String name, double outstanding){
         System.out.println("name: " + name);
         System.out.println("amount: " + outstanding);
     }
+
 }
 
 class Order {
--- src/test/java/com/tws/refactoring/extract_method/OwingPrinterTest.java
+++ src/test/java/com/tws/refactoring/extract_method/OwingPrinterTest.java
@@ -6,6 +6,8 @@ import org.junit.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.junit.Assert.*;
 
@@ -23,9 +25,23 @@ public class OwingPrinterTest {
         System.setOut(originalOut);
     }
 
-    @Test
+    /*@Test
     public void printOwing() {
-//        System.out.print("hello");
-//        assertEquals("hello", outContent.toString());
+        System.out.print("hello");
+        assertEquals("hello", outContent.toString());
+    }*/
+
+    @Test
+    public void printOwing1() {
+        OwingPrinter owingPrinter = new OwingPrinter();
+        owingPrinter.printOwing("TYG", new ArrayList<>(Arrays.asList(new Order[]{new Order(1.0), new Order(2.0), new Order(10.0)})));
+
+        String expected = "*****************************\r\n" +
+                "****** Customer totals ******\r\n" +
+                "*****************************\r\n" +
+                "name: TYG\r\n" +
+                "amount: 13.0\r\n";
+        assertEquals(expected,outContent.toString());
+
     }
 }
